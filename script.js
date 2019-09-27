5205a02840101aa081c8aec55bcd664b2c66244e
Elliot Allard <eallard@andrew.cmu.edu>
2019-09-27 14:52:28 -0400

    working shop page, except for home button

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function() {
+  	var elementTarget = document.getElementById("section2");
+  	if (window.scrollY > 800) {
+
+      	document.getElementById("navbarSupportedContent")
+     	const li = document.createElement('LI');
+	    const txt = document.createTextNode("HOME");
+      	li.className = 'nav-item';
+	    if (recoveredList[i].deleted) {
+	      li.style.display = 'none';
+	    }
+	    li.appendChild(txt);
+	    ul.appendChild(li);
+  }
+});
