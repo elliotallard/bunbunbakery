507bed16f952e6e309fc795e1a8fcb8cea12e211
Elliot Allard <eallard@andrew.cmu.edu>
2019-10-02 12:40:40 -0400

    button functionality working

--- script.js
+++ script.js
@@ -15,47 +15,24 @@
 // });
 
 
-// // taken from W3Schools, https://www.w3schools.com/howto/howto_js_active_element.asp
-
-// // Get the container element
-// var btns = document.getElementsByClassName("flex-container");
-
-// // Get all buttons with class="btn" inside the container
-// // var btns = document.getElementById("how-many").getElementsByClassName("flex-container");
-
-// // Loop through the buttons and add the active class to the current/clicked button
-
-
-// const list = document.getElementById('how-many');
-
-// document.getElementsByClassName('flex-container').addEventListener(
-//   'click',
-//   ev => {
-
-//   	console.log(0);
-//     for (var i = 0; i < btns.length; i++) {
-// 		console.log(0);
-// 	    var current = document.getElementsByClassName("active");
-// 	    current[0].className = current[0].className.replace(" active", "");
-// 	    this.className += " active";
-//   };
-// });
-
-
-
+// taken from W3Schools, https://www.w3schools.com/howto/howto_js_active_element.asp
 
 // Get the container element
-// const btns = document.getElementsByClassName("flex-container");
+let numberSection = document.getElementById("how-many");
+
+// Get all buttons with class="btn" inside the container
+let numBtns = numberSection.getElementsByClassName("flex-container");
+
+//then do the same thing for frosting.
+let frostingSection = document.getElementById('how-frosting');
+let frostBtns = frostingSection.getElementsByClassName('myButton');
 
 
 
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < 4; i++) {
-	var btns = document.getElementsByClassName("flex-container");
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-
+const markActive = (rowOfButtons, scope) => {
+	for (var i = 0; i < rowOfButtons.length; i++) {
+  rowOfButtons[i].addEventListener("click", function() {
+    var current = scope.getElementsByClassName("active");
     // If there's no active class
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active", "");
@@ -63,7 +40,24 @@ for (var i = 0; i < 4; i++) {
 
     // Add the active class to the current/clicked button
     this.className += " active";
+    console.log(current[0].className);
   });
 }
+}
+
+
+markActive(numBtns, numberSection);
+markActive(frostBtns, frostingSection);
+
+
+
+
+
+
+
+
+
+
+
 
 
